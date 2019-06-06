58168c0dc4ac1c3a1906345d0b0d603e5becf9e9
Johnny <tjohnny93@berkeley.edu>
2019-06-06 11:21:15 -0700

    Modify users_controller

--- music_app/app/controllers/users_controller.rb
+++ music_app/app/controllers/users_controller.rb
@@ -1,16 +1,29 @@
 class UsersController < ApplicationController
     
     def create
-        
+        @user = User.new(user_params)        
+        if @user.save
+            login_user!(@user)
+            redirect_to user_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
     end
 
     def new
-
+        @user = User.new
+        render :new
     end
 
     def show
-
+        render :show
     end
 
-    
+    private
+
+    def user_params
+        params.requires(:user).permit(:email, :password)
+    end
+
 end
