f0de56d6b44782ae68bc9bd140e01e5eec8d4a7d
Johnny <tjohnny93@berkeley.edu>
2019-06-06 11:29:10 -0700

    Add #new,#destroy,#create in SessionsController

--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+    def new
+        render :new
+    end
+
+    def destroy
+        logout_user!
+        redirect_to new_session_url
+    end
+
+    def create
+        user = User.find_by_credentials(
+            params[:user][:email]
+            params[:user][:password]
+        )
+
+        if user.nil?
+            flash.now[:errors] = ["Incorrect email and/or password"]
+            render :new
+        else
+            login_user!(user)
+            redirect_to user_url
+        end
+    end
+
 end
