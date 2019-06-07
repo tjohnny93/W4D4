c9b027f336fb90c986b1dc3f71e078aaa2860851
Johnny <tjohnny93@berkeley.edu>
2019-06-06 17:35:42 -0700

    Add album table

--- music_app/db/schema.rb
+++ music_app/db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_161658) do
+ActiveRecord::Schema.define(version: 2019_06_07_003031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.integer "band_id", null: false
+    t.string "title", null: false
+    t.integer "year", null: false
+    t.boolean "live", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_albums_on_band_id", unique: true
+    t.index ["title"], name: "index_albums_on_title", unique: true
+  end
+
+  create_table "bands", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
